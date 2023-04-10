4a9d47d50b557b1ce62da9f3ea6753001538a200
hoang1789 <nguyenngochoang89@gmail.com>
2023-04-10 15:59:27 +0700

    change sthing home

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 
+
 def home(request):
-   text = """<h1>our mind blowing home page</h1>"""
-   return HttpResponse(text)
+    text = """<h1>our mind blowing home page is up and running</h1>"""
+    return HttpResponse(text)
